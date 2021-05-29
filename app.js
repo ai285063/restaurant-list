085cc99f30a8280c7abf87caec89e7e2fc828577
Meg <ai285063@gmail.com>
2021-05-29 23:48:24 +0800

    feat: add create restaurant function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const restaurantList = require('./models/seeds/restaurant.json')
 const Restaurant = require('./models/restaurant')
 
@@ -20,11 +21,24 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const newRestaurant = req.body
+  return Restaurant.create(Object.assign(newRestaurant))
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 app.get('/restaurants/:id', (req, res) => {
