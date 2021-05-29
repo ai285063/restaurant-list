a70c90607a0d5d9d440ef5e0448960dbe0e8fe1f
Meg <ai285063@gmail.com>
2021-05-29 18:53:13 +0800

    feat: add main.hbs & index.hbs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const restaurantList = require('./models/seeds/restaurant.json')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('index', { restaurants: restaurantList.results })
 })
 
-app.listen(3001, () => {
-  console.log('App is running on http://localhost:3001')
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
 })
