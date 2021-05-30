eda797c10327273ff16ae471a61a5490f1f1e75c
Meg <ai285063@gmail.com>
2021-05-30 16:38:45 +0800

    feat: modify restaurantSeeder & /models/restaurant

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,11 +1,48 @@
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const Restaurant = require('../restaurant')
+const User = require('../user')
 const db = require('../../config/mongoose')
 const { results: data } = require('./restaurant.json')
 
-db.once('open', () => {
-  for (let i = 0; i < data.length; i++) {
-    const { id, ...rest } = data[i]
-    Restaurant.create({ ...rest })
+const SEED_USER = [
+  {
+    email: 'user1@example.com',
+    password: '12345678'
+  },
+  {
+    email: 'user2@example.com',
+    password: '12345678'
   }
-  console.log('done')
+]
+
+db.once('open', () => {
+  return Promise.all(Array.from(
+    { length: SEED_USER.length }, (_, i) => {
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(SEED_USER[i].password, salt))
+        .then(hash => User.create({
+          email: SEED_USER[i].email,
+          password: hash
+        }))
+        .then(user => {
+          const userId = user._id
+          return Promise.all(Array.from(
+            { length: 3 },
+            (_, j) => Restaurant.create({ ...data[j + i * 3], userId })
+          ))
+        })
+        .then(() => {
+          console.log(`User${i + 1} Seeder done.`)
+          if (i === (SEED_USER.length - 1)) {
+            console.log('Seeder load done.')
+            process.exit()
+          }
+        })
+    }
+  ))
 })
